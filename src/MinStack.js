d927c9068296745b40b5ced5f385dc92484a7263
majeedabudbay <48244398+majeedabudbay@users.noreply.github.com>
2020-05-31 09:34:01 +0300

    add files

--- src/MinStack.js
+++ src/MinStack.js
@@ -4,10 +4,83 @@
 
 
 
+class MinStack extends Stack{
+
+    constructor(){
+        super()
+        this.min = null
+    }
+
+    push(x){
+        if(!this.min){
+            this.min = x
+        }
+        this.stack[this.length] = x
+        this.length++
+        if(x < this.min){
+            this.min = x
+        }
+        
+    }
+
+    pop(){
+        if (this.isEmpty()) {
+            this.min = null
+            return null
+        }
+        this.length--
+        const toDelete = this.stack[this.length]
+        if(toDelete == this.min){
+            this.min = null
+        }
+        
+        this.stack.splice(this.length, 1)
+        for(let num of this.stack){
+            
+            if(!this.min){
+                this.min = num
+            }
+            else if(num < this.min){
+                this.min = num
+            }
+        }
+        
+        return toDelete
+       
+        
+    }
+
+    getMin(){
+        return this.min
+    }
+}
+
+
 
 
 
 //You can paste the test code from the lesson below to test your solution
+
+
+let ms = new MinStack()
+ms.push(17)
+ms.push(12)
+ms.push(31)
+console.log(ms.getMin())    //12
+ms.pop()
+ms.pop()
+ms.print()
+console.log(ms.getMin())    //17
+ms.pop()
+console.log(ms.getMin())    //null
+ms.push(19)
+ms.push(21)
+console.log(ms.getMin())    //19
+ms.push(3)
+console.log(ms.getMin())    //3
+ms.pop()
+ms.pop()
+console.log(ms.getMin())    //19
 
 
 
--- src/Queue.js
+++ src/Queue.js
@@ -1,6 +1,36 @@
 /* Write your class below */
 
+class Queue{
 
+    constructor(){
+        this.queue = []
+        this.length = 0
+    }
+
+    enqueue(x){
+        this.queue.unshift(x)
+        this.length++
+    }
+
+    isEmpty(){
+        return this.length == 0
+    }
+
+    peek(){
+        return this.queue[this.length - 1] || null
+    }
+
+    dequeue(){
+        const toDelete =  this.queue[this.length - 1] || null
+        this.queue.splice(this.length - 1, 1)
+        this.length--
+        return toDelete
+    }
+
+    print(){
+        console.log(this.queue)
+    }
+}
 
 
 
@@ -10,6 +40,18 @@
 //You can paste the test code from the lesson below to test your solution
 
 
+let q = new Queue()
+console.log(q.isEmpty())    //true
+q.print()                   //[]
+q.enqueue(2)
+console.log(q.isEmpty())    //false
+q.enqueue(4)
+q.print()                   //[4,2]
+console.log(q.peek())       //2
+q.dequeue()
+q.dequeue()
+console.log(q.peek())       //null
+console.log(q.isEmpty())    //true
 
 
 /* Do not remove the exports below */
